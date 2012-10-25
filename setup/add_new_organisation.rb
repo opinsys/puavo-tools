afbaf308f623955afa39dfb4f6664000adc57f50
Jouni Korhonen <jouni.korhonen@opinsys.fi>
2012-10-25 17:36:29 +0300

    Require readline library

--- setup/add_new_organisation.rb
+++ setup/add_new_organisation.rb
@@ -9,6 +9,7 @@ $LOAD_PATH.unshift( File.join( File.dirname(__FILE__), 'lib' ) )
 require 'rubygems'
 require 'active_ldap'
 require 'optparse'
+require 'readline'
 
 options = {}
 a = OptionParser.new do |opts|
